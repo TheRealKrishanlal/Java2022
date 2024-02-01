0a3ff02a3c9b54cfa8161793660d455f6874e446
sandr <sandrak9@gmail.com>
2024-02-01 12:43:19 +0200

    initialized fields in CrewMember.java

--- src/CrewMember.java
+++ src/CrewMember.java
@@ -1,2 +1,15 @@
 public class CrewMember {
+
+    private String fullname;
+    private int crewID;
+    private String department;
+    int  numPromotedCrew = 0;
+
+    public CrewMember(String inF, int inCID, String inDT){
+
+        this.fullname = inF;
+        this.crewID = inCID;
+        this.department = inDT;
+
+    }
 }
